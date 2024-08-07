6044f116056eb68f43dd625b01f4ad32a923c961
Alan Davidson <alan@viam.com>
2024-08-07 10:20:30 -0400

    the linter is wrong, but I can't argue with it

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ package main
 import (
 	"context"
 
+	"go.viam.com/utils"
 	"pinctrl/pi5"
 
 	"go.viam.com/rdk/components/board"
 	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
-	"go.viam.com/utils"
 )
 
 func main() {
