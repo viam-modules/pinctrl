1f8fe22ff85365abb2ae4704bfb756cb17df2cb8
Alan Davidson <alan@viam.com>
2024-08-22 15:04:35 -0400

    [RSDK-8494] Validate PWM values before setting them (#20)
    I kinda want to wait for https://github.com/viamrobotics/rdk/pull/4309 to be available in RDK, and then use that helper function instead of copying and pasting it. but then we'd need to wait another week for this PR. So, we're using the latest version of RDK, rather than a published version.
    
    Tried on my rpi5: works great!
    
    
    * add bounds checking when setting PWM duty cycle
    
    * also add bounds checking on the frequency
    
    * 'go get' the latest RDK
    
    * use the helper function instead of copying and pasting it

--- pi5/gpio.go
+++ pi5/gpio.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pkg/errors"
 	"go.viam.com/utils"
 
+	"go.viam.com/rdk/components/board"
 	"go.viam.com/rdk/logging"
 )
 
@@ -316,6 +317,11 @@ func (pin *gpioPin) SetPWM(ctx context.Context, dutyCyclePct float64, extra map[
 	pin.mu.Lock()
 	defer pin.mu.Unlock()
 
+	dutyCyclePct, err := board.ValidatePWMDutyCycle(dutyCyclePct)
+	if err != nil {
+		return err
+	}
+
 	pin.pwmDutyCyclePct = dutyCyclePct
 	return pin.startSoftwarePWM()
 }
@@ -332,6 +338,10 @@ func (pin *gpioPin) PWMFreq(ctx context.Context, extra map[string]interface{}) (
 func (pin *gpioPin) SetPWMFreq(ctx context.Context, freqHz uint, extra map[string]interface{}) error {
 	pin.mu.Lock()
 	defer pin.mu.Unlock()
+
+	if freqHz < 1 {
+		return errors.New("must set PWM frequency to a positive value")
+	}
 
 	pin.pwmFreqHz = freqHz
 	return pin.startSoftwarePWM()
